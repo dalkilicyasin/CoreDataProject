65e43eb80672c13074d7d5dab6641312e062f6c1
Yasin <72190310+dalkilicyasin@users.noreply.github.com>
2022-11-25 11:08:19 +0300

    Add files via upload

---
+++ CoreDataProject/CoreDataProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataProject
+//
+//  Created by Yasin Dalkilic on 25.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
