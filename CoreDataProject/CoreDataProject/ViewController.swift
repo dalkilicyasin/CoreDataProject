a3e79157cbd1d08225526e254d2ab28980afad75
yasindalkilic <ysndlklc1989@icloud.com>
2022-11-25 11:34:36 +0300

    commit

--- CoreDataProject/CoreDataProject/ViewController.swift
+++ CoreDataProject/CoreDataProject/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("yasin first commit")
+        print("second commit")
     }
 
 
