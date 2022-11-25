4649f2621872c55d66b284ce89fc4059d15761ab
yasindalkilic <ysndlklc1989@icloud.com>
2022-11-25 11:52:01 +0300

    no message

--- CoreDataProject/CoreDataProject/ViewController.swift
+++ CoreDataProject/CoreDataProject/ViewController.swift
@@ -12,7 +12,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
         print("yasi")
+        print("yasin first commit")
+        print("second commit")
+
     }
 
 
